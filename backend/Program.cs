06c358b1f13374cf6a8dd92ca89861170fb7ea08
Daniel <danielbraithwaite73@gmail.com>
2026-04-06 20:27:48 -0600

    testing backend redeploy

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
 
+var test = "Hello world!";
 // Deploy test 2 I am making this very significant change wowowoww
 var builder = WebApplication.CreateBuilder(args);
 
